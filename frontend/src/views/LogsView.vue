b9d3334b924acd6641fbaa0d758db8c6102b414d
Kadats <marcelo.kadats@gmail.com>
2026-02-14 17:23:23 -0300

    feat: add LogTerminal component with WebSocket integration and update dependencies

--- frontend/src/views/LogsView.vue
+++ frontend/src/views/LogsView.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="p-4">
-    <div class="rounded-md bg-black p-4 font-mono text-green-400">
-      &gt; Aguardando conexao...
+    <div class="h-[calc(100vh-100px)] rounded-md bg-black p-2">
+      <LogTerminal />
     </div>
   </div>
 </template>
+
+<script setup>
+import LogTerminal from '../components/LogTerminal.vue';
+</script>
